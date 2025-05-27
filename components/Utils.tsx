ff15bebd4489a9ae3718fbbbe6ad2b576f1f1b6a
Fransiskus Richard <fransiskus.richard@pactindo.com>
2025-05-27 10:33:33 +0700

    add logo in login

--- components/Utils.tsx
+++ components/Utils.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import LogoImage from "@/public/logo.png";
+
 export const Logo = () => {
-  return <div className="logo">FINBEST</div>;
+  return (
+    <div className="justify-items-center">
+      <h1 className="text-7xl">SIPPAD</h1>
+      <Image src={LogoImage} alt="LOGO" />
+    </div>
+  );
 };
